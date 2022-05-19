8aae6a3e507b8b7cf5f2bbf5068a34a424f14fd8
Sylvia <834939448@qq.com>
2022-05-19 23:26:42 +1000

    new project

---
+++ src/pages/details/mockup.ts
@@ -0,0 +1,100 @@
+// 用户评论
+export const commentMockData = [
+    {
+      author: "Han Solo",
+      avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+      content: `Mervat and Saeid were fantastic. They made us feel comfortable all day and made sure we had the best possible experience. Mervat was a wonderful teacher and always helped us get the best picture. I highly recommend this tour! If you want a comfortable and informative experience, book this trip!`,
+      createDate: "2020-01-05",
+    },
+    {
+      author: "Han Solo",
+      avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+      content: `We had a bad start (2 hours late)due to a glitch in the computer system of the tour company; however our tour guide Sam made up all the bad feelings that we had at the beginning, Sam was very knowledgeable and an expert taking pictures at the end we were very happy and I will recommend the tour to anyone.`,
+      createDate: "2020-02-06",
+    },
+    {
+      author: "Han Solo",
+      avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+      content: `We had ahmed for the day showing us throughout luxor and he was amazing. Very knowledgeable on every location throughout the tour and we learned alot about the backstories of the hieroglyphics. He even sat down and ate lunch with us to tell us about egpyt aside from tour. The driver was very accommodating as well (wish i remember his name lol). Overall a great experience of luxor`,
+      createDate: "2020-01-04",
+    },
+    {
+      author: "Han Solo",
+      avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+      content: `The tour was excellent from the start. Mustapha was full of knowledge and very engaging, and was happy to step back to allow us take all the time we wanted to enjoy the sites and take pictures (we never felt rushed). I highly recommend Viator and would always ask for Mustapha when we’re in Luxor as we had a great experience with him……the temples are breathtaking, pictures and movies don’t do it justice. Highly recommend.`,
+      createDate: "2020-02-05",
+    },
+  ];
+
+  export const productDetail = 
+  {
+    "id": "fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1",
+    "title": "Private Tour to Giza Pyramids and The Egyptian Museum",
+    "description": "Explore Cairo’s Egyptian Museum and Giza Pyramids on a private full-day tour. Visit two of Cairo’s star turns with no need to plan transport or tickets",
+    "price": 1199.999,
+    "originalPrice": 11999.99,
+    "discountPresent": 0.1,
+    "rating": 3.5,
+    "travelDays": "EightPlus",
+    "tripType": "HotelAndAttractions",
+    "departureCity": "Beijing",
+    "createTime": "0001-01-01T00:00:00",
+    "updateTime": null,
+    "departureTime": null,
+    "features": "<div class=\"bd\"><p style=\"text-align:center\"><strong><span style=\"color:#ffffff;font-size:24px\"><a href=\"http://vacations.ctrip.com/tour/detail/p19441039s2.html\" target=\"_blank\"></a></span></strong><strong style=\"color:rgb(192, 0, 0)\"><span style=\"font-size:24px\">Private Tour to Giza Pyramids and The Egyptian Museum</span></strong></p><p style=\"text-align:center\"><span style=\"color:rgb(192, 0, 0)\"><strong><span style=\"color:rgb(192, 0, 0);font-size:14px\"><strong style=\"color:rgb(34, 34, 34);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;font-size:14px;text-align:center;white-space:normal;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);font-size:24px;position:static;height:auto\">This private tour eliminates the hassles and provides a personal guide to proffer observations and background. </span></strong></span></strong></span></p><p style=\"text-align:center\"><span style=\"color:rgb(0, 0, 0)\"><span style=\"font-size:14px;background-color:rgb(255, 255, 255)\">Explore Cairo’s Egyptian Museum and Giza Pyramids on a private full-day tour</span><span style=\"font-size:14px;background-color:rgb(255, 255, 255)\"></span></span><br></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);text-align:center;font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><br></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Upgrade】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Visit two of Cairo’s star turns with no need to plan transport or tickets</span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><strong style=\"float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">See the pyramids, sphinx, and museum’s must-sees with a guide for insight</span></strong></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【People Limit】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Only small and private tours！</span></span><span style=\"color:rgb(192, 0, 0);position:static;height:auto\"><br>☆【Luxury Hotels】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Relax with admission tickets, lunch, and hotel pickup and drop-off included</span></span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Long Exploration】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Explore Cairo’s Egyptian Museum and Giza Pyramids on a private full-day tour</span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Features】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Visiting Cairo’s Giza Pyramids and Egyptian Museum is tricky by yourself</span></span><span style=\"color:rgb(192, 0, 0);position:static;height:auto\"><br>☆【Special Arrangement】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">you’ll need to haggle with cabs and do your own navigation and research. </span></span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Upgrated Transport】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Classy and cozy new buses with limited on borading people！</span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><strong style=\"float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">&nbsp;Get a bird's eye view of Luxor's ancient sites from aboard a hot air balloon.</span></strong></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Themes】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\"> Your driver will pick you up and take you to the launch site in an air-conditioned vehicle<br><span style=\"color:rgb(255, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【New Playground】</span><span style=\"color:rgb(0, 0, 0);position:static;height:auto\">where you'll attend a safety briefing before getting ready to fly</span></span><br></span></span><span style=\"color:rgb(255, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Special Views】</span><span style=\"color:rgb(0, 0, 0);position:static;height:auto\">You'll then head up to the sky, with a local guide on board to point out sights and tell you about their history and relevance<br><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Important Sites】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Take a hot air balloon ride over Luxor</span></span></span></span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(255, 0, 0);position:static;height:auto\"><span style=\"color:rgb(0, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\"><span style=\"color:rgb(0, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0)\">☆【Professonal Guide】</span><span style=\"position:static;height:auto\">An English-speaking guide will provide commentary</span><br></span></span></span></span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Lower Price】</span><span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Save money by opting for a group ride</span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(255, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Convience】</span><span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Round-trip transfers from your hotel are included</span></span><br></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\"><strong style=\"color:rgb(34, 34, 34);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;font-size:14px;white-space:normal;position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">&nbsp;&nbsp;Followed by a visit to the Temple of Queen Hatshepsut that known as El Dir El Bahari </span></strong></span></p><p><span style=\"font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;font-size:14px;background-color:rgb(255, 255, 255);color:rgb(192, 0, 0);position:static;height:auto\">☆【Drive To Airport】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Later you will be driven back to your hotel in Luxor. It even can be ended in Luxor airport </span></span><br><span style=\"font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;font-size:14px;background-color:rgb(255, 255, 255);color:rgb(192, 0, 0);position:static;height:auto\">☆【Tips Included】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">All tips are included in the price<br><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Better Choice】<span style=\"color:rgb(0, 0, 0);position:static;height:auto\">Finish your day tour by visiting the Temple Of Luxor,！</span></span></span></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(255, 0, 0);position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Safe Trip】</span><span style=\"color:rgb(0, 0, 0);position:static;height:auto\"></span></span>If you want to see the best of what Luxor has to offer, this tour is a safe bet</span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><span style=\"color:rgb(192, 0, 0);position:static;height:auto\">☆【Facilities】</span>All WIFIs included in the all trip！</span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"><br></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"></span></p><p style=\"margin-top:0px;margin-bottom:0px;padding:0px;color:rgb(34, 34, 34);font-size:14px;white-space:normal;background-color:rgb(255, 255, 255);font-family:Arial, &quot;Lucida Grande&quot;, Verdana, &quot;Microsoft YaHei&quot;, hei;float:none;position:static;height:auto\"><span style=\"position:static;height:auto\"></span></p><p><img imageid=\"21313898\" src=\"//dimg04.c-ctrip.com/images/30050o000000f6rizD972.jpg\" data-src=\"//dimg04.c-ctrip.com/images/30050o000000f6rizD972.jpg\" title=\"A-NEW.jpg\" imageauthorize=\"21313898图片有效-有效期\" style=\"opacity: 1;\"> &nbsp; <br></p></div>",
+    "fees": "<div class=\"bd\"><dl class=\"mod_info_box\"><dt>Fees Included</dt><dd><div class=\"tour_description_table_box\"><table class=\"tour_description_table\"><tbody><tr><th>Transportation</th><td><p>All the Flights</p></td></tr><tr><th>Accomodation</th><td><p></p><p>Double Rooms in Hotel</p></td></tr><tr><th>Food</th><td><p>For the meals listed in the itinerary, please refer to the itinerary recommendation / arrangement for details. Please refer to the regulations of the airline for whether the air meal is charged</p></td></tr><tr><th>Tickets</th><td><p>All included in the tickets</p></td></tr><tr><th>Tour Guide</th><td><p>English-speaker team leaders</p></td></tr><tr><th>Children tickets</th><td><p>Free fro age 2-6</p></td></tr></tbody></table></div></dd></dl><dl class=\"mod_info_box\"><dt>Not Included Fees</dt><dd><div class=\"tour_description_table_box\"><table class=\"tour_description_table\"><tbody><tr><th>Children Extra</th><td><p>The price of children in the team is the price excluding breakfast. If you need to occupy the bed, please select the option of making up for the difference in children's occupied bed in the follow-up page of reservation</p></td></tr><tr><th>Addition</th><td><p>Customs levy on entry-exit personal belongings; Check in fee and storage fee for excess baggage; Personal consumption such as laundry, haircut, telephone, fax, pay TV, drinks, tobacco and alcohol in the hotel; Car service during free activities</p><p></p></td></tr></tbody></table></div></dd></dl><dl class=\"mod_info_box\"><dd><div class=\"tour_self_table_box\"></div></dd></dl></div>",
+    "touristRoutePictures": [
+        {
+            "id": 1,
+            "url": "https://z3.ax1x.com/2020/12/15/rMQOIJ.jpg",
+            "touristRouteId": "fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1"
+        },
+        {
+            "id": 2,
+            "url": "https://z3.ax1x.com/2020/12/15/rMQRaQ.jpg",
+            "touristRouteId": "fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1"
+        },
+        {
+            "id": 3,
+            "url": "https://z3.ax1x.com/2020/12/15/rMQhPs.jpg",
+            "touristRouteId": "fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1"
+        },
+        {
+            "id": 4,
+            "url": "https://z3.ax1x.com/2020/12/15/rMQIx0.jpg",
+            "touristRouteId": "fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1"
+        }
+    ],
+    "links": [
+        {
+            "href": "http://123.56.149.216:8080/api/TouristRoutes/fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1",
+            "rel": "self",
+            "method": "GET"
+        },
+        {
+            "href": null,
+            "rel": "update",
+            "method": "PUT"
+        },
+        {
+            "href": null,
+            "rel": "partially_update",
+            "method": "PATCH"
+        },
+        {
+            "href": null,
+            "rel": "delete",
+            "method": "DELETE"
+        },
+        {
+            "href": "http://123.56.149.216:8080/api/touristRoutes/fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1/pictures",
+            "rel": "get_pictures",
+            "method": "GET"
+        },
+        {
+            "href": "http://123.56.149.216:8080/api/touristRoutes/fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1/pictures",
+            "rel": "create_picture",
+            "method": "POST"
+        }
+    ]
+}
